dca0d51e1c19fff60e3e5e6e410615e101f689da
Gabriel Starosky <gabrielstarosky@gmail.com>
2023-04-09 20:58:05 -0300

    Implement not equal operator

--- tests/test_pyspark_mock/test_sql/test_data_frame.py
+++ tests/test_pyspark_mock/test_sql/test_data_frame.py
@@ -268,3 +268,22 @@ class TestDataFrame(unittest.TestCase):
         expected_results = [False, True, False, False, True, False, False]
 
         self.assertListEqual(list(df.pd_df.result), expected_results)
+
+    def test_not_equal_operator(self):
+        observations = [
+            (1, 0),
+            (1, 1),
+            (0, 1),
+            (-2, 0),
+            (-2, -2),
+            (4, 2),
+            (4, 6)
+        ]
+
+        columns = ['n1', 'n2']
+        df = self._createDataFrame(observations, columns)
+        df = df.withColumn('result', F.col('n1') != F.col('n2'))
+
+        expected_results = [True, False, True, True, False, True, True]
+
+        self.assertListEqual(list(df.pd_df.result), expected_results)
